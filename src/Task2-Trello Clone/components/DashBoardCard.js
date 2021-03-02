8e22f1d1fcb6dddd74ddc7a6bb99b47fdc3102a0
atul.rana <atul.rana@zopsmart.com>
2021-03-02 12:58:41 +0530

    update

--- src/Task2-Trello Clone/components/DashBoardCard.js
+++ src/Task2-Trello Clone/components/DashBoardCard.js
@@ -1,50 +1,50 @@
-import React, { useState } from 'react'
-import { MdDelete } from 'react-icons/md'
-import axios from 'axios'
-import config from '../config/config'
-import PropTypes from 'prop-types'
-import '../task2.css'
+import React, { useState } from "react";
+import { MdDelete } from "react-icons/md";
+import axios from "axios";
+import config from "../config/config";
+import PropTypes from "prop-types";
+import "../task2.css";
 
 function DashBoardCard({ card, getCardsForList, boardId }) {
-  const [currentCardName, updateCurrentCard] = useState(card.name)
-  const [loadingUpdate, setLoadingUpdate] = useState(false)
-  const [cardState, setCardState] = useState(true)
+  const [currentCardName, updateCurrentCard] = useState(card.name);
+  const [loadingUpdate, setLoadingUpdate] = useState(false);
+  const [cardState, setCardState] = useState(true);
 
   const deleteCard = async (cardId) => {
-    setLoadingUpdate(true)
+    setLoadingUpdate(true);
     await axios.delete(
       `https://api.trello.com/1/cards/${cardId}?key=${config.key}&token=${config.token}`
-    )
+    );
     const getCards = await axios.get(
       `https://api.trello.com/1/boards/${boardId}/cards?key=${config.key}&token=${config.token}`
-    )
-    getCardsForList(getCards.data)
-    setLoadingUpdate(false)
-  }
+    );
+    getCardsForList(getCards.data);
+    setLoadingUpdate(false);
+  };
   const updateCard = async (e, cardId) => {
-    e.preventDefault()
-    setLoadingUpdate(true)
+    e.preventDefault();
+    setLoadingUpdate(true);
     await axios.put(
       `https://api.trello.com/1/cards/${cardId}?key=${config.key}&token=${config.token}&name=${currentCardName}`
-    )
+    );
 
     const getCards = await axios.get(
       `https://api.trello.com/1/boards/${boardId}/cards?key=${config.key}&token=${config.token}`
-    )
-    setLoadingUpdate(false)
-    setCardState(true)
-    getCardsForList(getCards.data)
-  }
+    );
+    setLoadingUpdate(false);
+    setCardState(true);
+    getCardsForList(getCards.data);
+  };
   const handleKeyDown = (e, cardId) => {
-    if (e.key === 'Enter') {
-      e.preventDefault()
-      updateCard(e, cardId)
+    if (e.key === "Enter") {
+      e.preventDefault();
+      updateCard(e, cardId);
     }
-  }
+  };
 
   const handleCardChange = (e) => {
-    updateCurrentCard(e.target.value)
-  }
+    updateCurrentCard(e.target.value);
+  };
   const cardData = cardState ? (
     <h1 className="card-name">{currentCardName}</h1>
   ) : (
@@ -54,19 +54,13 @@ function DashBoardCard({ card, getCardsForList, boardId }) {
       data-testid="updateForm"
       onSubmit={(e) => updateCard(e, card.id)}
     >
-      {loadingUpdate ? (
-        <div className="update-card-loading">
-          <img src="/loading.gif" />
-        </div>
-      ) : (
-        <input
-          className="current-card-title"
-          data-testid="updateCardInputField"
-          value={currentCardName}
-          onKeyDown={(e) => handleKeyDown(e, card.id)}
-          onChange={(e) => handleCardChange(e)}
-        ></input>
-      )}
+      <input
+        className="current-card-title"
+        data-testid="updateCardInputField"
+        value={currentCardName}
+        onKeyDown={(e) => handleKeyDown(e, card.id)}
+        onChange={(e) => handleCardChange(e)}
+      ></input>
 
       <input
         type="submit"
@@ -75,7 +69,7 @@ function DashBoardCard({ card, getCardsForList, boardId }) {
         data-testid="updateCardSubmitButton"
       />
     </form>
-  )
+  );
   return (
     <div>
       <div className="cards" key={card.id}>
@@ -83,10 +77,16 @@ function DashBoardCard({ card, getCardsForList, boardId }) {
           <div
             data-testid="makeCardEditable"
             onClick={() => {
-              setCardState(false)
+              setCardState(false);
             }}
           >
-            {cardData}
+            {loadingUpdate ? (
+              <div className="update-card-loading">
+                <img src="/loading.gif" />
+              </div>
+            ) : (
+              cardData
+            )}
           </div>
         </div>
         <div className="card-icons">
@@ -97,12 +97,12 @@ function DashBoardCard({ card, getCardsForList, boardId }) {
         </div>
       </div>
     </div>
-  )
+  );
 }
 DashBoardCard.propTypes = {
   card: PropTypes.object,
   getCardsForList: PropTypes.func,
-  boardId: PropTypes.string
-}
+  boardId: PropTypes.string,
+};
 
-export default DashBoardCard
+export default DashBoardCard;
